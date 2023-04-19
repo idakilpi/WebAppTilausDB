4075c82020c635936aec7275dcf1c3d6d5e3e438
Ida Yliluoma <Ida.Yliluoma@student.careeria.fi>
2023-04-19 19:58:41 +0300

    update

---
+++ WebAppTilausDB/ViewModels/TilausSummaryData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+
+namespace WebAppTilausDB.ViewModels
+{
+    public class TilausSummaryData
+    {
+        public int TilausID{ get; set; }
+        public Nullable<System.DateTime> TilausPvm { get; set; }
+        public Nullable<System.DateTime> ToimitusPvm { get; set; }
+
+        public Nullable<int> AsiakasID { get; set; }
+
+        public string Toimitusosoite { get; set; }
+    }
+}
---
+++ WebAppTilausDB/ViewModels/TilausrivitModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using WebAppTilausDB.Models;
+
+namespace WebAppTilausDB.ViewModels
+{
+    public class TilausrivitModel
+    {
+        public int TilausriviID { get; set; }
+        public Nullable<int> TilausID { get; set; }
+        public Nullable<int> TuoteID { get; set; }
+
+        [Display(Name = "Määrä")]
+        public Nullable<int> Maara { get; set; }
+
+        public Nullable<decimal> Ahinta { get; set; }
+
+        public virtual Tilaukset Tilaukset { get; set; }
+        public virtual Tuotteet Tuotteet { get; set; }
+    }
+}
